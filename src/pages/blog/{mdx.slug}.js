9de7a258413f5919c99e1896177acccde67cafa1
Camila Queiroz <camilaqueiroz@mbp-de-camila.home>
2021-08-31 19:53:13 -0300

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,25 +1,48 @@
 import React from "react";
-import Layout from "../../components/layout";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
+import Layout from "../../components/layout";
+import { creditsLink } from "./blog.module.css";
+
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <small>
+        <a
+          className={creditsLink}
+          href={data.mdx.frontmatter.hero_image_credit_link}
+          target="_blank"
+        >
+          Photo credit:{" "}
+          <span>{data.mdx.frontmatter.hero_image_credit_text}</span>
+        </a>
+      </small>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: { eq: $id }) {
+  query ($slug: String) {
+    mdx(slug: { eq: $slug }) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `;
